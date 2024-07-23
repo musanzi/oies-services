114136f593bdc84aa592a1611ec124c2a49d2924
Wilfried Musanzi <musanziw.m@gmail.com>
2024-07-23 15:59:12 +0200

    feat: add hero

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
+  imports: [HeroComponent],
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {}
