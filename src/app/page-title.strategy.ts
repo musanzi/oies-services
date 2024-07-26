fde8db23f0e66f1cd9ab17c59325623ba92e8163
Wilfried Musanzi <musanziw.m@gmail.com>
2024-07-26 09:33:39 +0200

    chore: fix page title

--- src/app/page-title.strategy.ts
+++ src/app/page-title.strategy.ts
@@ -11,7 +11,7 @@ export class PageTitleStrategy extends TitleStrategy {
   override updateTitle(routerState: RouterStateSnapshot) {
     const title = this.buildTitle(routerState);
     if (title !== undefined) {
-      this.title.setTitle(`Woies | ${title}`);
+      this.title.setTitle(`Oies | ${title}`);
     }
   }
 }
